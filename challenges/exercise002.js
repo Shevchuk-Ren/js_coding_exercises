5ae7325fdae5834886709cbb5ef1b3dfd942257a
Shevchuk-Ren <shevchuk_re@ukr.net>
2022-12-05 15:42:08 +0000

    exercise002 is done

--- challenges/exercise002.js
+++ challenges/exercise002.js
@@ -1,24 +1,41 @@
 export function getFillings(sandwich) {
   if (sandwich === undefined) throw new Error("ingredients is required");
-  // Your code here!
+  return sandwich.fillings;
 }
 
 export function isFromManchester(person) {
   if (person === undefined) throw new Error("person is required");
-  // Your code here!
+  
+  return person.city === "Manchester" ? true : false;
 }
 
 export function getBusNumbers(people) {
   if (people === undefined) throw new Error("people is required");
-  // Your code here!
+  
+  const peopleOnBus = people / 40;
+  const fullBuses = peopleOnBus.toString().split('.')[0];
+  const emptySeats = peopleOnBus.toString().split('.')[1];
+
+  if(Number.parseInt(emptySeats) > 0) {
+    return Number.parseInt(fullBuses) + 1;
+  } else {
+    return Number.parseInt(fullBuses);
+  }
 }
 
 export function countSheep(arr) {
   if (arr === undefined) throw new Error("arr is required");
-  // Your code here!
+  const cheepArr = arr.filter((sheep) => sheep === "sheep");
+  return cheepArr.length;
 }
 
 export function hasMPostCode(person) {
   if (person === undefined) throw new Error("person is required");
-  // Your code here!
+  
+  if(person.address.city === "Manchester" && 
+     person.address.postCode.startsWith('M')) {
+    return true;
+  } else {
+    return false;
+  }
 }
