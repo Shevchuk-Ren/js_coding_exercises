23ddf3680b951ec7d17ca9719ff85b07fb4a908b
Shevchuk-Ren <shevchuk_re@ukr.net>
2022-12-05 17:14:05 +0000

    added excercise003

--- challenges/exercise003.js
+++ challenges/exercise003.js
@@ -1,26 +1,43 @@
 export function getSquares(nums) {
   if (nums === undefined) throw new Error("nums is required");
-  // Your code here!
+  return nums.map((number) => number**2);
 }
 
 export function camelCaseWords(words) {
   if (words === undefined) throw new Error("words is required");
-  // Your code here!
+  
+  let camelCaseWord = words[0];
+  for (let i = 1; i < words.length; i++) {
+    camelCaseWord += words[i].charAt(0).toUpperCase() + words[i].slice(1)
+  }
+  return camelCaseWord;
 }
 
 export function getTotalSubjects(people) {
   if (people === undefined) throw new Error("people is required");
-  // Your code here!
+  
+  let countSubjects = 0;
+  for (let i = 0; i < people.length; i++) {
+    countSubjects += people[i].subjects.length;
+  }
+  return countSubjects;
 }
 
 export function checkIngredients(menu, ingredient) {
   if (menu === undefined) throw new Error("menu is required");
   if (!ingredient) throw new Error("ingredient is required");
-  // Your code here!
+  
+  let isIngridient = false;
+  for (let i = 0; i < menu.length; i++) {
+    if(menu[i].ingredients.includes(ingredient)) {
+      isIngridient = true;
+    }  
+  }
+  return isIngridient;
 }
 
 export function duplicateNumbers(arr1, arr2) {
   if (arr1 === undefined) throw new Error("arr1 is required");
   if (arr2 === undefined) throw new Error("arr2 is required");
-  // Your code here!
+  
 }
